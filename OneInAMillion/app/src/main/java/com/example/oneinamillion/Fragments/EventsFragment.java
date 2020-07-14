43831c10e9e0daa66f005bde19e5eea4283d9c07
Gianna Torpey <giannaaa@fb.com>
2020-07-14 08:25:07 -0700

    Added Collapsible RVs to My Events Page

--- OneInAMillion/app/src/main/java/com/example/oneinamillion/Fragments/EventsFragment.java
+++ OneInAMillion/app/src/main/java/com/example/oneinamillion/Fragments/EventsFragment.java
@@ -2,22 +2,56 @@ package com.example.oneinamillion.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.oneinamillion.R;
 
 
 public class EventsFragment extends Fragment {
 
-
+    RecyclerView rvCreated;
+    RecyclerView rvUpcoming;
+    TextView tvMyUpcomingEvents;
+    TextView tvMyOrganizedEvents;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvCreated = view.findViewById(R.id.rvCreated);
+        rvUpcoming = view.findViewById(R.id.rvUpcoming);
+        tvMyOrganizedEvents = view.findViewById(R.id.tvMyOrganizedEvents);
+        tvMyUpcomingEvents = view.findViewById(R.id.tvMyUpcomingEvents);
+        tvMyOrganizedEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rvCreated.setVisibility(View.VISIBLE);
+    }
+        });
+        tvMyUpcomingEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rvUpcoming.setVisibility(View.VISIBLE);
+            }
+        });
+
+        queryEvents();
+    }
+
+    private void queryEvents() {
+
     }
 }
--- OneInAMillion/app/src/main/java/com/example/oneinamillion/Fragments/SearchFragment.java
+++ OneInAMillion/app/src/main/java/com/example/oneinamillion/Fragments/SearchFragment.java
@@ -1,66 +1,52 @@
 package com.example.oneinamillion.Fragments;
 
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.oneinamillion.MainActivity;
 import com.example.oneinamillion.R;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SearchFragment extends Fragment {
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private LocationRequest mLocationRequest;
+    Location mCurrentLocation;
+    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private final static String KEY_LOCATION = "location";
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     public SearchFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
