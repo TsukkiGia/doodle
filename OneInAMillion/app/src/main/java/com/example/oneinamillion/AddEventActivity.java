8ce00a4af839088cae81e8e3b77ecab59768dc28
Gianna Torpey <giannaaa@fb.com>
2020-07-15 09:10:50 -0700

    Fixed bug on uploading image and added place picker for Compose Event page

--- OneInAMillion/app/src/main/java/com/example/oneinamillion/AddEventActivity.java
+++ OneInAMillion/app/src/main/java/com/example/oneinamillion/AddEventActivity.java
@@ -1,20 +1,41 @@
 package com.example.oneinamillion;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.oneinamillion.Models.Event;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,22 +43,46 @@ import java.util.Date;
 public class AddEventActivity extends AppCompatActivity {
     EditText etEventName;
     EditText etEventDescription;
-    EditText etEventLocation;
+    Button btnPickAPlace;
     EditText etEventTime;
     EditText etEventDate;
     Button btnPost;
     Button btnUploadImage;
+    ImageView ivUploadedImage;
+    ParseFile file;
+    TextView tvLocation;
+    Button btnRepickAPlace;
+
     public static final String TAG = "AddEvent";
+    public final static int PICK_PHOTO_CODE = 1046;
+    public final static int PICK_LOCATION_CODE = 20020;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
         etEventName = findViewById(R.id.etEventName);
+        tvLocation = findViewById(R.id.tvLocation);
         etEventDescription = findViewById(R.id.etEventDescription);
-        etEventLocation = findViewById(R.id.etEventLocation);
         etEventTime = findViewById(R.id.etEventTime);
         etEventDate = findViewById(R.id.etEventDate);
+        ivUploadedImage = findViewById(R.id.ivUploadedImage);
+        btnPickAPlace = findViewById(R.id.btnPickAPlace);
+        btnPickAPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEventActivity.this,MapActivity.class);
+                startActivityForResult(intent, PICK_LOCATION_CODE);
+            }
+        });
+        btnRepickAPlace = findViewById(R.id.btnRepickAPlace);
+        btnRepickAPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEventActivity.this,MapActivity.class);
+                startActivityForResult(intent, PICK_LOCATION_CODE);
+            }
+        });
         btnPost = findViewById(R.id.btnPost);
         btnUploadImage = findViewById(R.id.btnUploadImage);
         btnPost.setOnClickListener(new View.OnClickListener() {
@@ -46,26 +91,105 @@ public class AddEventActivity extends AppCompatActivity {
                 saveEvent();
             }
         });
+        btnUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPickPhoto(view);
+            }
+        });
     }
-
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
     private void saveEvent() {
-        Event event = new Event();
-        event.setEventName(etEventName.getText().toString());
-        event.setDescription(etEventDescription.getText().toString());
-        event.setDate(etEventDate.getText().toString());
-        event.setTime(etEventTime.getText().toString());
-        event.setLocation(new ParseGeoPoint(10,2));
-        event.setOrganizer(ParseUser.getCurrentUser());
-        event.saveInBackground(new SaveCallback() {
+        //final ParseFile parseFile = new ParseFile(photoFile);
+        file.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if (e!=null) {
-                    Log.e(TAG,"Error while saving",e);
-                    Toast.makeText(AddEventActivity.this, "Error while saving",Toast.LENGTH_SHORT).show();
-                    return;
+                    Log.e(TAG,"help",e);
                 }
-                Log.i(TAG,"Save successful");
+                else {
+                    Event event = new Event();
+                    event.setEventName(etEventName.getText().toString());
+                    event.setDescription(etEventDescription.getText().toString());
+                    event.setDate(etEventDate.getText().toString());
+                    event.setTime(etEventTime.getText().toString());
+                    event.setLocation(new ParseGeoPoint(10,2));
+                    event.setOrganizer(ParseUser.getCurrentUser());
+                    event.setImage(file);
+                    event.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e!=null) {
+                                Log.e(TAG,"Error while saving",e);
+                                Toast.makeText(AddEventActivity.this, "Error while saving",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            Log.i(TAG,"Save successful");
+                            finish();
+                        }
+                    });
+                }
             }
         });
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+            // Load the selected image into a preview
+            ivUploadedImage.setImageBitmap(selectedImage);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            // Compress image to lower quality scale 1 - 100
+            selectedImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] image = stream.toByteArray();
+            // Create the ParseFile
+            file = new ParseFile("picture.png", image);
+        }
+
+        if (requestCode == PICK_LOCATION_CODE && resultCode == Activity.RESULT_OK) {
+            double latitude = data.getDoubleExtra("latitude", 0);
+            double longitude = data.getDoubleExtra("longitude", 0);
+            btnPickAPlace.setVisibility(View.GONE);
+            btnRepickAPlace.setVisibility(View.VISIBLE);
+            tvLocation.setVisibility(View.VISIBLE);
+            String name = data.getStringExtra("name");
+            tvLocation.setText(name);
+
+            //Toast.makeText(AddEventActivity.this,name,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 }
---
+++ OneInAMillion/app/src/main/java/com/example/oneinamillion/MapActivity.java
@@ -0,0 +1,70 @@
+package com.example.oneinamillion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+import okhttp3.Headers;
+
+public class MapActivity extends AppCompatActivity {
+    public static final String TAG = "MapsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        String apiKey = getString(R.string.api_key);
+
+        /**
+         * Initialize Places. For simplicity, the API key is hard-coded. In a production
+         * environment we recommend using a secure mechanism to manage API keys.
+         */
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+// Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.toString()+ ", " + place.getLatLng());
+                //Toast.makeText(MapActivity.this,"Place: " + place.getName() + ", " + place.getLatLng(),Toast.LENGTH_SHORT).show();
+                setResult(Activity.RESULT_OK,
+                        new Intent().putExtra("name",place.getName()).putExtra("latitude", place.getLatLng().latitude).putExtra("longitude", place.getLatLng().longitude));
+                finish();
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+    }
+}
